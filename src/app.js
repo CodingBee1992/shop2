08bd52865c6571499438e0a0c2c7275bb2707851
Przemek <mcsteamy24@gmail.com>
2025-03-17 11:59:19 +0100

    toggle menu

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
 import './sass/style.scss'
 
-import './components/samllimg.js'
+// import './components/samllimg.js'
+
+
+
+const navbar = document.querySelector('.navbar')
+const btn = document.querySelector('.fa-outdent')
+const hideMenuBtn = document.querySelector('.close')
+const toggleMenu = ()=>{
+    navbar.classList.toggle('show-menu')
+}
+
+btn.addEventListener('click',toggleMenu)
+hideMenuBtn.addEventListener('click',toggleMenu)
